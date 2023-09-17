f4142038ecd38017e8e45c4d88e813c25cd893e4
samiras <samiras@localhost>
2023-09-16 20:06:58 -0400

    commit 09.16.23

--- alerts/src/main/java/com/safetynet/alerts/repository/DataFileAccess.java
+++ alerts/src/main/java/com/safetynet/alerts/repository/DataFileAccess.java
@@ -1,7 +1,6 @@
 package com.safetynet.alerts.repository;
 
 import java.util.List;
-
 import com.safetynet.alerts.model.Firestations;
 import com.safetynet.alerts.model.MedicalRecords;
 import com.safetynet.alerts.model.Person;
